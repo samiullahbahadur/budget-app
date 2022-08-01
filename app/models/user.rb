6dd9f426eb496be64726d793a02f739e6c4410a7
samiullah <bahadursamiullah@gmail.com>
2022-08-01 16:58:03 +0430

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+     validates :firstname, presence:true 
+     validates :lastname, presence:true 
+     has_many :groups
+     has_many  :entities
 end
