4ea5136a77c6e7007c2e840a9377a3ef769659e2
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-02 00:58:25 +0430

    upadate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  root 'groups#index'
+  resources :users, only: :index do
+    resources :groups, only: [:index, :new, :create] do
+      resources :entities, only: [:index, :new, :create]
+    end
+  end
 end
