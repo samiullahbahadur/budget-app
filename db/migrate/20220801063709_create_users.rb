4e80cf7f23bc428572e6851f7509e47205190d79
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-02 00:59:25 +0430

    Update user migration

--- db/migrate/20220801063709_create_users.rb
+++ db/migrate/20220801063709_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :firstname
+      t.string :lastname
 
       t.timestamps
     end
