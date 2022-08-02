2421ec81b4f39c99753bccddcd41abce478996bc
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-03 01:10:09 +0430

    Add create action etity

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,10 +1,31 @@
 class EntitiesController < ApplicationController
   def index
-
+   @groups = Group.find(params[:group_id])
+    @entity = @groups.entities
+      @total = 0
+      @entity.each do |price|
+      @total += price.amount
+      end
   end
 
   def new
   @user=current_user
   @entity=Entity.new
+  end
+
+  def create 
+    @entity =Entity.new(entity_params)
+    @entity.user=current_user
+    @entity.group=Group.find(params[:group_id])
+    if @entity.save
+      flash[:notice]= "Entity Successfully Created"
+      redirect_to user_group_entities_path
+    else
+      flash[:alert]="something was wrong please try again"
+      render 'new'
+    end
+  end
+  def entity_params 
+    params.require(:entity).permit(:name, :amount)
   end
 end
