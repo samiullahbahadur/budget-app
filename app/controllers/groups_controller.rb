06049d8c15ef95aa1840716ed4ac93199d8e75ed
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-04 00:02:52 +0430

    update entity controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,37 +1,30 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    @group = Group.all
     @user = current_user
+    @groups = Group.includes([image_attachment: :blob]).where(user: @user).order('created_at DESC')
   end
 
   def new
+    @groups = Group.new
     @user = current_user
-    @group = Group.new
+
   end
 
   def create
-    @group = Group.new(group_params)
-    @group.user = current_user
-    if @group.save
-      flash[:notice] = "Category successfully created"
+    @groups = Group.new(group_params)
+    @groups.user = current_user
+    if @groups.save
+      flash[:notice] = "Category added successfuly!"
       redirect_to user_groups_path
     else
-      render "new"
-      flash[:alert] = "somethings wrong"
-    end
-  end
-
-  def destroy
-    @group = Group.find(params[:id])
-    if @group.destroy
-      flash[:notice] = "Category successfully Deleted"
-      redirect_to user_groups_path
+      flash[:alert] = "Category declined!"
+      render 'new'
     end
   end
 
   private
-
   def group_params
     params.require(:group).permit(:name, :image)
   end
