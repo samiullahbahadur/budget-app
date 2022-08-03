64b79fc1ff6298a86495204766c1997dda26be00
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-04 00:02:00 +0430

    update  all action to groups

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,11 +1,8 @@
 class EntitiesController < ApplicationController
   def index
+    @user = current_user
     @groups = Group.find(params[:group_id])
-    @entity = @groups.entities
-    @total = 0
-    @entity.each do |price|
-      @total += price.amount
-    end
+    @entity = @groups.entities.order('created_at DESC')
   end
 
   def new
@@ -13,20 +10,21 @@ class EntitiesController < ApplicationController
     @entity = Entity.new
   end
 
-  def create
+  def create 
     @entity = Entity.new(entity_params)
     @entity.user = current_user
     @entity.group = Group.find(params[:group_id])
     if @entity.save
-      flash[:notice] = "Entity Successfully Created"
+      flash[:notice] = "Transaction added successfuly!"
       redirect_to user_group_entities_path
     else
-      flash[:alert] = "something was wrong please try again"
+      flash[:alert] = "Transaction declined!"
       render 'new'
     end
   end
-
-  def entity_params
+  
+  private
+  def entity_params 
     params.require(:entity).permit(:name, :amount)
   end
 end
