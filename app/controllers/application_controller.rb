478cc27efc60f852c479a40f5b055877f7145314
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-04 00:01:26 +0430

    Add authenticate_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
-
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  protected
-
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u|
-      u.permit(:firstname, :lastname, :email, :password, :password_confirmation)
-    }
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :avatar)
+    end
   end
+
+private
+def authenticate_user!
+  if user_signed_in?
+    super
+  else
+    redirect_to splashs_index_path
+  end
+end
 end
