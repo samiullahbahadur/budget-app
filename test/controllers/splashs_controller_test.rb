944428f7a4d68ab0ff67db4feb461ee1826d96ec
samiullah <bahadursamiullah@gmail.com>
2022-08-04 17:22:14 +0430

    implement unit and integration test

--- test/controllers/splashs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SplashsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get splashs_index_url
-    assert_response :success
-  end
-end
