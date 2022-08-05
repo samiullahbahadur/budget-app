ad206fc3ac311087833351b31c5e3017eac5e327
samiullahbahadur <bahadursamiullah@gmail.com>
2022-08-05 11:07:06 +0430

    Fix linters

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,12 +1,10 @@
-
 Devise.setup do |config|
-  
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   require 'devise/orm/active_record'
 
   config.case_insensitive_keys = [:email]
-  
+
   config.strip_whitespace_keys = [:email]
 
   config.skip_session_storage = [:http_auth]
@@ -24,5 +22,4 @@ Devise.setup do |config|
   config.reset_password_within = 6.hours
 
   config.sign_out_via = :delete
-
 end
